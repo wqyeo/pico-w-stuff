7768f0dc0ee151f8c2067e290af8e584777428d2
Yeo Wen Qin, Kaynn <KaynnCahya@outlook.com>
2023-09-22 14:33:32 +0800

    added lab3

--- lab3/interrupts_lab.c
+++ lab3/interrupts_lab.c
@@ -1,40 +1,69 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "pico/time.h"
+#include "pico/types.h"
 
 #define BTN_PIN 15
 
-uint64_t current_timer = 0;
-uint64_t last_timestamp = 0;
+volatile uint32_t timer_seconds = 0;
+volatile bool b_stopwatch_running = false;
 
-bool repeating_timer_callback(struct repeating_timer *t) {
-    uint64_t current_timestamp = time_us_64();
-    uint64_t difference = current_timestamp - last_timestamp;
+// Next signal trigger has to be after 700ms, otherwise its ignored.
+const uint32_t debounce_trigger = 700;
+uint32_t last_interrupt_time = 0;
 
-    current_timer += difference;
+bool timer_callback(struct repeating_timer *timer) {
+    if (b_stopwatch_running) {
+        // Since timer runs on 1 second callback,
+        // we can safely dictate that 1 second has passed...
+        // (Ignoring micro-second differences due to execution delays, etc)
+        timer_seconds++;
+        printf("Timer: %u sec\n", timer_seconds);
+    }
 
-    // Convert it to actual seconds and print...
-    printf("%i\n", current_timer / 1000000);
-
-    last_timestamp = current_timestamp;
     return true;
 }
 
+void start_stopwatch(){
+    b_stopwatch_running = true;
+    printf("Stopwatch started!\n");
+}
+
+void stop_stopwatch() {
+    b_stopwatch_running = false;
+    printf("Stopwatch stopped!\n");
+}
+
 void gpio_callback(uint gpio, uint32_t events) {
-    current_timer = 0;
+    uint32_t time_now = to_ms_since_boot(get_absolute_time());
+
+    // Ignore callback signals that happens too rapidly.
+    // (Debouncing)
+    if (time_now - last_interrupt_time > debounce_trigger) {
+        if ((events & GPIO_IRQ_EDGE_FALL) && !b_stopwatch_running) {
+            start_stopwatch();
+        } else if ((events & GPIO_IRQ_EDGE_RISE) && b_stopwatch_running) {
+            stop_stopwatch();
+        }
+        timer_seconds = 0;
+    }
+    last_interrupt_time = time_now;
 }
 
 int main() {
-    char received_char;
     stdio_usb_init();
 
-    // Set interrupt on button. 
-    gpio_set_irq_enabled_with_callback(BTN_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
+    gpio_init(BTN_PIN);
+    gpio_set_dir(BTN_PIN, GPIO_IN);
+    gpio_pull_up(BTN_PIN);
+
+    // Connect button PIN to ground to start timer,
+    // disconnect to start.
+    gpio_set_irq_enabled_with_callback(BTN_PIN, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
     
-    last_timestamp = time_us_64();
-    
-    // Set repeating timer...
     struct repeating_timer timer;
-    add_repeating_timer_ms(-500, repeating_timer_callback, NULL, &timer);
+    add_repeating_timer_ms(-1000, timer_callback, NULL, &timer);
+
     while (true);
 }
